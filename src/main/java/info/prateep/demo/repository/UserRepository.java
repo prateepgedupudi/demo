6f44888926eb910bdfbace6be3a139aa78c069f0
Prateep Gedupudi <prateep.gedupudi@gmail.com>
2019-09-05 20:46:30 +0530

    Initial commit...

---
+++ src/main/java/info/prateep/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package info.prateep.demo.repository;
+
+import info.prateep.demo.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ src/main/java/info/prateep/demo/web/UserController.java
@@ -0,0 +1,27 @@
+package info.prateep.demo.web;
+
+import info.prateep.demo.entity.User;
+import info.prateep.demo.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+}
